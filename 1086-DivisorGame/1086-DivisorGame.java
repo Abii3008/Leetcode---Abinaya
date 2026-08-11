bcbfc82125eeb32b2c8624960b5eb42d9907b66c
Abinaya Anbazhagan <abinayaanbu1030@gmail.com>
2026-08-11 16:06:51 +0530

    Create 1086-DivisorGame

---
+++ 1086-DivisorGame/1086-DivisorGame.java
@@ -0,0 +1,10 @@
+// Last updated: 11/08/2026, 16:06:48
+class Solution {
+    public boolean divisorGame(int n) {
+        if (n%2==0) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
