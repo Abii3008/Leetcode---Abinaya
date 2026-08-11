98d11f06c6bc1dc1094ac15572e73aa9420674af
Abinaya Anbazhagan <abinayaanbu1030@gmail.com>
2026-08-11 16:15:32 +0530

    Create 83-RemoveDuplicatesFromSortedList

---
+++ 83-RemoveDuplicatesFromSortedList/83-RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,14 @@
+// Last updated: 11/08/2026, 16:15:29
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode curr = head;
+        while (curr != null && curr.next != null) {
+            if (curr.val == curr.next.val) {
+                curr.next = curr.next.next; 
+            } else {
+                curr = curr.next;
+            }
+        }
+        return head;
+    }
+}
