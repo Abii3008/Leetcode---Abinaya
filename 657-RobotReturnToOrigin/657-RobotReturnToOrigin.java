0f73f497772a8fc974eeff396e27746700961204
Abinaya Anbazhagan <abinayaanbu1030@gmail.com>
2026-08-11 16:08:55 +0530

    Create 657-RobotReturnToOrigin

---
+++ 657-RobotReturnToOrigin/657-RobotReturnToOrigin.java
@@ -0,0 +1,13 @@
+// Last updated: 11/08/2026, 16:08:52
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x=0,y=0;
+        for(char m:moves.toCharArray()) {
+            if(m=='R') x++;
+            else if (m=='L') x--;
+            else if (m=='U') y++;
+            else if (m=='D') y--;
+        }
+        return x==0 && y==0;
+    }
+}
