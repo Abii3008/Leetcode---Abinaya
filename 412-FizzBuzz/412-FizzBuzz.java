7404e09fc606e0544306d444158348d943fe3d81
Abinaya Anbazhagan <abinayaanbu1030@gmail.com>
2026-08-11 16:11:04 +0530

    Create 412-FizzBuzz

---
+++ 412-FizzBuzz/412-FizzBuzz.java
@@ -0,0 +1,11 @@
+// Last updated: 11/08/2026, 16:11:01
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List ans = new ArrayList<>();
+        for(int i=1;i<=n;i++) {
+            ans.add(
+                i%15==0?"FizzBuzz": i%5==0?"Buzz" : i%3==0?"Fizz" : String.valueOf(i));
+        }
+        return ans;
+    }
+}
