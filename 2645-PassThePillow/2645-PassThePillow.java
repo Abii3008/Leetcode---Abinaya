4e65ea6a86fc9d30c9afc84942a3b7fd544b8a6d
Abinaya Anbazhagan <abinayaanbu1030@gmail.com>
2026-08-11 16:03:19 +0530

    Create 2645-PassThePillow

---
+++ 2645-PassThePillow/2645-PassThePillow.java
@@ -0,0 +1,12 @@
+// Last updated: 11/08/2026, 16:03:16
+class Solution {
+    public int passThePillow(int n, int time) {
+        int cycle = 2 * (n - 1);
+        int t = time % cycle;
+        if (t < n) {
+            return 1 + t;
+        } else {
+            return n - (t - (n - 1));
+        }
+    }
+}
