857162889f4e8619241ddcd17abbe311cd3310a8
Abinaya Anbazhagan <abinayaanbu1030@gmail.com>
2026-08-11 16:01:49 +0530

    Create 4352-WeightedSumOfATree

---
+++ 4352-WeightedSumOfATree/4352-WeightedSumOfATree.java
@@ -0,0 +1,26 @@
+// Last updated: 11/08/2026, 16:01:46
+class Solution {
+    public long weightedSum(int[] parent, int[] nums) {
+        int n=parent.length,h=1;
+        int[] d=new int[n];
+        d[0]=1;
+        for(int i=1;i<n;i++){
+            int x=i,cnt=0;
+            while(x!=-1 && d[x]==0){
+                cnt++;
+                x=parent[x];
+            }
+            int base=x==-1?0:d[x];
+            x=i;
+            while(d[x]==0) {
+                d[x]=base+cnt--;
+                x=parent[x];
+            }
+            h=Math.max(h,d[i]);
+        }
+        long ans=0;
+        for(int i=0;i<n;i++)
+            ans+=(long)nums[i]*(h-d[i]+1);
+        return ans;
+    }
+}
