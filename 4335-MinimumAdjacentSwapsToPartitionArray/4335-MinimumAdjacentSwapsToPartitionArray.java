4b49b7d861897611a525e446c0177a789f449867
Abinaya Anbazhagan <abinayaanbu1030@gmail.com>
2026-08-11 16:02:05 +0530

    Create 4335-MinimumAdjacentSwapsToPartitionArray

---
+++ 4335-MinimumAdjacentSwapsToPartitionArray/4335-MinimumAdjacentSwapsToPartitionArray.java
@@ -0,0 +1,22 @@
+// Last updated: 11/08/2026, 16:02:02
+class Solution {
+    public int minAdjacentSwaps(int[] nums, int a, int b) {
+        long swaps=0;
+        long mod = 1_000_000_007;
+        long count0 = 0;
+        long count1 = 0;
+        long count2 = 0;
+        for(int num:nums) {
+            if(num <a) {
+                swaps = (swaps+count1+count2)%mod;
+                count0++;
+            } else if(num<=b) {
+                swaps = (swaps+count2)%mod;
+                count1++;
+            } else {
+                count2++;
+            }
+        }
+        return (int) swaps;
+    }
+}
