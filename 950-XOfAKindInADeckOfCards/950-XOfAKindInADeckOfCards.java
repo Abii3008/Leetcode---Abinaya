2e184d96685711306e215cf4bd037a4a53a08079
Abinaya Anbazhagan <abinayaanbu1030@gmail.com>
2026-08-11 16:07:23 +0530

    Create 950-XOfAKindInADeckOfCards

---
+++ 950-XOfAKindInADeckOfCards/950-XOfAKindInADeckOfCards.java
@@ -0,0 +1,19 @@
+// Last updated: 11/08/2026, 16:07:20
+class Solution {
+    public boolean hasGroupsSizeX(int[] deck) {
+        int[] count = new int[10000];
+        for (int card : deck) {
+            count[card]++;
+        }
+        int gcd = 0;
+        for (int c : count) {
+            if (c > 0) {
+                gcd = gcd(gcd, c);
+            }
+        }
+        return gcd >= 2;
+    }
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+}
