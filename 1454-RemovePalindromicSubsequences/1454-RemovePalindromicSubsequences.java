cb00f45ee30bffc5a6fc4cf890fb7b9f16ddb9c8
Abinaya Anbazhagan <abinayaanbu1030@gmail.com>
2026-08-11 16:05:24 +0530

    Create 1454-RemovePalindromicSubsequences

---
+++ 1454-RemovePalindromicSubsequences/1454-RemovePalindromicSubsequences.java
@@ -0,0 +1,12 @@
+// Last updated: 11/08/2026, 16:05:21
+class Solution {
+    public int removePalindromeSub(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) return 2;
+            left++;
+            right--;
+        }
+        return 1;
+    }
+}
