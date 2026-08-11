6c922999bd6e44429a56484ef3897e08b0d75cae
Abinaya Anbazhagan <abinayaanbu1030@gmail.com>
2026-08-11 16:11:52 +0530

    Create 303-RangeSumQueryImmutable

---
+++ 303-RangeSumQueryImmutable/303-RangeSumQueryImmutable.java
@@ -0,0 +1,12 @@
+// Last updated: 11/08/2026, 16:11:50
+class NumArray {
+    int[] pre;
+    public NumArray(int[] nums) {
+        pre = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++)
+            pre[i + 1] = pre[i] + nums[i];
+    }
+    public int sumRange(int left, int right) {
+        return pre[right + 1] - pre[left];
+    }
+}
