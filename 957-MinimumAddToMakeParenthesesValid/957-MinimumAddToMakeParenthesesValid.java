12661f32c433e55bd521b2e0d07815b129b7dc43
Abinaya Anbazhagan <abinayaanbu1030@gmail.com>
2026-08-11 16:07:18 +0530

    Create 957-MinimumAddToMakeParenthesesValid

---
+++ 957-MinimumAddToMakeParenthesesValid/957-MinimumAddToMakeParenthesesValid.java
@@ -0,0 +1,19 @@
+// Last updated: 11/08/2026, 16:07:15
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int open = 0;
+        int add = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                open++;
+            } else {
+                if (open > 0) {
+                    open--;
+                } else {
+                    add++;
+                }
+            }
+        }
+        return add + open;
+    }
+}
