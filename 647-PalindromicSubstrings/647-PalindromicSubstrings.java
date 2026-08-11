faa2e7f7be923333f38174ea1290bf6dac30e632
Abinaya Anbazhagan <abinayaanbu1030@gmail.com>
2026-08-11 16:08:59 +0530

    Create 647-PalindromicSubstrings

---
+++ 647-PalindromicSubstrings/647-PalindromicSubstrings.java
@@ -0,0 +1,26 @@
+// Last updated: 11/08/2026, 16:08:56
+class Solution {
+    int check(String st,int i,int j) {
+        int c = 0;
+        int n = st.length();
+        while(i>=0 && j<n) {
+            if(st.charAt(i)==st.charAt(j)) {
+                c++;
+                i--;
+                j++;
+            }else{
+                break;
+            }
+        }
+        return c;
+    }
+    public int countSubstrings(String s) {
+        int ans = 0;
+        int n = s.length();
+        for(int i=0;i<n;i++) {
+            ans+=check(s,i,i);
+            ans+=check(s,i,i+1);
+        }
+        return ans;
+    }
+}
