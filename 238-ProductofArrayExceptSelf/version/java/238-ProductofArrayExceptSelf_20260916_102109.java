e5a888838d48c7316b9d3f128679fcd875cf9deb
Abinaya Anbazhagan <abinayaanbu1030@gmail.com>
2026-09-16 10:21:14 +0530

    Create 238-ProductofArrayExceptSelf

---
+++ 238-ProductofArrayExceptSelf/version/java/238-ProductofArrayExceptSelf_20260916_102109.java
@@ -0,0 +1,11 @@
+// Last updated: 16/09/2026, 10:21:09
+1class Solution {
+2    public String reverseWords(String s) {
+3        String[] str = s.trim().split("\\s+");
+4        String out = "";
+5        for(int i=str.length-1;i>0;i--) {
+6            out+=str[i]+ " ";
+7        }
+8        return out + str[0];
+9    }
+10}
